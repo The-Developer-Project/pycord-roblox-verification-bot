01eaa0c835de567c3c2b822905c2f2b6c349c18a
Galactic Apricot <87578551+GalacticApricot@users.noreply.github.com>
2023-03-30 21:30:34 +0100

    Create Verify.lua

---
+++ Verify.lua
@@ -0,0 +1,12 @@
+local HttpService = game:GetService("HttpService")
+local url = "https://TDPB.galacticapricot.repl.co/verify"
+game.Players.PlayerAdded:Connect(function(player)
+	player.Chatted:Connect(function(msg)
+		if msg == "/link" then
+			local tnum = tostring(math.random(1,10000000))
+			local token = tnum .. '+' .. player.Name
+			HttpService:PostAsync(url, token)
+			game.ReplicatedStorage.Verify:FireClient(player, tnum)
+		end
+	end)
+end)
